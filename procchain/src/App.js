22ff726d4a3dca2d96f90a410e7b4b7da0c4f64e
Harshita <h2010singh@gmail.com>
2020-01-13 19:22:45 +0530

    Init : LockScreen & Forgot Password

--- procchain/src/App.js
+++ procchain/src/App.js
@@ -8,17 +8,23 @@ import Login from './components/Login_Registeration/Login';
 import AboutUs from './components/LandingPage/AboutUs';
 import Services from './components/LandingPage/Services';
 import Register from './components/Login_Registeration/Registeration';
+import ForgotPassword from './components/Login_Registeration/ForgotPassword';
+import LockScreen from './components/Login_Registeration/LockScreen';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+
         <Route exact path="/" component = {LandingPage} />
         <Route exact path="/aboutUs" component = {AboutUs}/>
         <Route exact path="/services" component = {Services}/>
         <Route exact path="/auth-login" component = {Login} />
         <Route exact path="/team" component = {Team} />
         <Route exact path='/auth-register' component = {Register} />
+        <Route exact path='/forgot-password' component = {ForgotPassword} />
+        <Route exact path='/logged-out' component={LockScreen} />
+
       </BrowserRouter>
       
     </div>
