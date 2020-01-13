098ebe76e552d0498646538e17a3ee9112b858c4
Harshita <h2010singh@gmail.com>
2020-01-13 15:59:01 +0530

    Services

--- procchain/src/components/LandingPage/Services.js
+++ procchain/src/components/LandingPage/Services.js
@@ -2,58 +2,73 @@ import React, { Component } from 'react'
 import Service from '../common/LandingPage/Service';
 import HorizontalLine from '../common/HorizontalLine';
 
-const service = {
+const services = {
     sections : [
         [
             {
-                icon : "entypo-cog", 
-                heading : "Settings", 
-                para : "Fifteen no inquiry cordial so resolve garrets as. Impression was estimating surrounded solicitude indulgence son shy."
-            },
-            {
-                icon : "entypo-gauge", 
-                heading : "Dashboard", 
-                para : "On am we offices expense thought. Its hence ten smile age means. Seven chief sight far point any. Of so high into easy."
-            },
-            {
-                icon : "entypo-lifebuoy", 
-                heading : "24/7 Support", 
-                para : "Extremely eagerness principle estimable own was man. Men received far his dashwood subjects new."
-            },    
+                name : "GAIL Ltd.",
+                sectionService : [      
+                    {
+                        icon : "entypo-cog", 
+                        heading : "Settings", 
+                        para : "Fifteen no inquiry cordial so resolve garrets as. Impression was estimating surrounded solicitude indulgence son shy."
+                    },
+                    {
+                        icon : "entypo-gauge", 
+                        heading : "Dashboard", 
+                        para : "On am we offices expense thought. Its hence ten smile age means. Seven chief sight far point any. Of so high into easy."
+                    },
+                    {
+                        icon : "entypo-lifebuoy", 
+                        heading : "24/7 Support", 
+                        para : "Extremely eagerness principle estimable own was man. Men received far his dashwood subjects new."
+                    },
+                ]
+            }    
         ],
         [
             {
-                icon : "entypo-cog", 
-                heading : "Settings", 
-                para : "Fifteen no inquiry cordial so resolve garrets as. Impression was estimating surrounded solicitude indulgence son shy."
+                name : "Supplier",
+                sectionService : [
+                    {
+                        icon : "entypo-cog", 
+                        heading : "Settings", 
+                        para : "Fifteen no inquiry cordial so resolve garrets as. Impression was estimating surrounded solicitude indulgence son shy."
+                    },
+                    {
+                        icon : "entypo-gauge", 
+                        heading : "Dashboard", 
+                        para : "On am we offices expense thought. Its hence ten smile age means. Seven chief sight far point any. Of so high into easy."
+                    },
+                    {
+                        icon : "entypo-lifebuoy", 
+                        heading : "24/7 Support", 
+                        para : "Extremely eagerness principle estimable own was man. Men received far his dashwood subjects new."
+                    }
+                ]
             },
-            {
-                icon : "entypo-gauge", 
-                heading : "Dashboard", 
-                para : "On am we offices expense thought. Its hence ten smile age means. Seven chief sight far point any. Of so high into easy."
-            },
-            {
-                icon : "entypo-lifebuoy", 
-                heading : "24/7 Support", 
-                para : "Extremely eagerness principle estimable own was man. Men received far his dashwood subjects new."
-            }
         ],
         [
             {
-                icon : "entypo-cog", 
-                heading : "Settings", 
-                para : "Fifteen no inquiry cordial so resolve garrets as. Impression was estimating surrounded solicitude indulgence son shy."
+                name : "Shipments & Payments",
+                sectionService : [
+                    {
+                        icon : "entypo-cog", 
+                        heading : "Settings", 
+                        para : "Fifteen no inquiry cordial so resolve garrets as. Impression was estimating surrounded solicitude indulgence son shy."
+                    },
+                    {
+                        icon : "entypo-gauge", 
+                        heading : "Dashboard", 
+                        para : "On am we offices expense thought. Its hence ten smile age means. Seven chief sight far point any. Of so high into easy."
+                    },
+                    {
+                        icon : "entypo-lifebuoy", 
+                        heading : "24/7 Support", 
+                        para : "Extremely eagerness principle estimable own was man. Men received far his dashwood subjects new."
+                    }
+                ]
             },
-            {
-                icon : "entypo-gauge", 
-                heading : "Dashboard", 
-                para : "On am we offices expense thought. Its hence ten smile age means. Seven chief sight far point any. Of so high into easy."
-            },
-            {
-                icon : "entypo-lifebuoy", 
-                heading : "24/7 Support", 
-                para : "Extremely eagerness principle estimable own was man. Men received far his dashwood subjects new."
-            }
         ],
     ]
 }
@@ -65,14 +80,26 @@ export default class Services extends Component {
                 <div className="container">
                     <h1 style={{textAlign: "center"}}>SERVICES</h1>
                     <br/>
-                    {service.sections.map((section, sectionIndex) => {
+                    {services.sections.map((section, sectionIndex) => {
                         return (
                             <div className="row vspace" key={sectionIndex}>
+                                {/* <h3>{this.section}</h3> */}
                                 {section.map((service, index) => {
-                                    return <Service
-                                        {...service}
-                                        key={index}
-                                    />
+                                    // return <Service
+                                    //     {...service}
+                                    //     key={index}
+                                    // />
+                                    return (
+                                        <div key = {index}>
+                                            <h3 style={{textAlign: "center"}}>{service.name}</h3>
+                                            {service.sectionService.map((sec, index) => {
+                                                return <Service 
+                                                    {...sec}
+                                                    key = {index}
+                                                />
+                                            })}
+                                        </div>
+                                    )
                                 })}
                             </div>
                         )
