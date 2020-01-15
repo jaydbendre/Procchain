521d5b0fc65fda513279594d13748f9ebe83a2ae
jaydbendre <2017.jay.bendre@ves.ac.in>
2020-01-15 15:39:19 +0530

    Hi

--- ProcchainBackend/Backend/urls.py
+++ ProcchainBackend/Backend/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns =[
     path('',views.index,name = "index"),
     path('login/', views.login ,name = "login"),
-    path('user/<int:pk>',views.UserView.as_view()),
-    path('tender_info/<int:tender_id>',views.TenderView.as_view())
+    path('render_file_upload/' , views.render_file , name = "render_upload"),
+    path('file_upload/' , views.tender_file_upload , name = "file_upload")
+    # path('user/<int:pk>',views.UserView.as_view()),
+    # path('tender_info/<int:tender_id>',views.TenderView.as_view())
  ] 
