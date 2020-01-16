fc2597eccdf829b533b21e3923e0aa4cc943cfbf
Harshita <h2010singh@gmail.com>
2020-01-16 11:17:19 +0530

    Mail for OTP

--- ProcchainBackend/Backend/views.py
+++ ProcchainBackend/Backend/views.py
@@ -4,8 +4,9 @@ from django.db import connection
 from django.contrib.auth import authenticate
 from django.conf import settings
 from django.views.decorators.csrf import csrf_protect
-from django.core.mail import send_mail
+from django.core.mail import send_mail, send_mass_mail
 from django.template.loader import get_template
+from django.template import Context
 from jose import jws
 
 from rest_framework import status ,exceptions
@@ -210,14 +211,17 @@ def sendOTP(request):
         #     return JsonResponse({"otp" : otp, "value" : val})
         # else :
         val = str(random.randint(1,9))+str(random.randint(1,9))+str(random.randint(1,9))+str(random.randint(1,9))+str(random.randint(1,9))+str(random.randint(1,9))
-        otp = "Your OTP is : "+val
+        otp = "Your OTP is : " + val
+        t = get_template("mail_otp.html").template
+        c = Context({"otp" : otp}, autoescape = True)
+        # html = render_to_string("mail_otp.html", {'otp': otp}, c).strip()
         send_mail(
             "OTP for user #{}".format(request.session["uid"]),
             otp,
-            "2017.jay.bendre@ves.ac.in",
-            ['2017.harshita.singh@ves.ac.in'],
+            "wehire.sight@gmail.com",
+            ['2017.harshita.singh@ves.ac.in', '2017.jay.bendre@gmail.com', '2017.sumedh.ghavat@gmail.com', '2017.vignesh.pillai@gmail.com'],
             fail_silently=False,
-            # html_message=t.render()
+            html_message=t.render( context = c)
         )
         
         return JsonResponse({"otp" :otp})
