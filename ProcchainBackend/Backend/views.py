92575edb0dd68e6a33283e30be42200c7f2ab60b
AishwaryaANair <2017.aishwarya.nair@ves.ac.in>
2020-01-17 11:07:21 +0530

    'Update'

--- ProcchainBackend/Backend/views.py
+++ ProcchainBackend/Backend/views.py
@@ -512,7 +512,7 @@ def tender_file_upload(request) :
             }
             file_hash = str(file_hash)
             cursor.execute('INSERT INTO tender(tender_id,file_path,file_hash,uploaded_at,uploaded_by) values({},"{}","{}","{}",{})'.format(tender_id+1,file_path,file_hash,datetime.datetime.now(),uid))
-        return HttpResponse(str(tender_hash))
+        return JsonResponse({'tenderHash':tender_hash})
 
 def make_bids(request , tender_id): 
     with connection.cursor() as cursor : 
